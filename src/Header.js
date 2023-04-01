46733f65411d0194783255715771f6844d9f4d19
guillermojh <ing.guillermoh@gmail.com>
2023-04-01 18:11:48 -0300

    1.2

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
   return (
     <div id='cabecera'>
         <img className='logo' src={process.env.PUBLIC_URL + "/logo192.png"} alt= 'mi logo' />
-        <h3 className='mensaje'>Bienvenido a mi Buscador De Usuarios</h3>
+        <h3 className='mensaje'>Bienvenido a mi Buscador de Usuarios</h3>
     </div>
   )
 }
